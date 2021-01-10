6df399af4daea22f5bb5825c4e6ed2ae6e59c732
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-01-10 23:37:31 +0900

    add: docker用設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,4 +48,9 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
+
+  server: {
+    port: 3000, // デフォルト: 3000
+    host: '0.0.0.0', // デフォルト: localhost
+  },
 }
