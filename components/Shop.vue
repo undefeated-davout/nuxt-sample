e777f6ef96b3fd3a2e6c1126f0349861d6d95f8b
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-01-11 17:40:10 +0900

    add: ショップリストページ

---
+++ components/Shop.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="shop-card">
+    <div><img class="shop-image" :src="$props.image" alt="" /></div>
+    <div class="shop-info">
+      <div class="shop-name">{{ $props.shopName }}</div>
+      <div class="shop-score">{{ $props.score }}</div>
+      <div class="shop-description">{{ $props.description }}</div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    shopName: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+<style>
+.shop-card {
+  width: 300px;
+  margin: 8px;
+  box-shadow: 1px 1px 4px 1px rgb(218, 218, 218);
+  display: inline-block;
+  color: #333;
+}
+
+.shop-image {
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+}
+
+.shop-info {
+  padding: 4px;
+}
+
+.shop-name {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.shop-score {
+  text-align: right;
+}
+
+.shop-description {
+  margin-top: 12px;
+  font-size: 14px;
+}
+</style>
---
+++ pages/shop/list.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="content-title">人気のお店</div>
+    <div class="shop-list">
+      <div v-for="shop in shops" :key="shop.index">
+        <Shop
+          :image="shop.image"
+          :shop-name="shop.shopName"
+          :score="shop.score"
+          :description="shop.description"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export interface Shop {
+  image?: string
+  shopName?: string
+  score?: Number
+  description?: string
+}
+
+export default Vue.extend({
+  data: () => ({
+    shops: [] as any,
+  }),
+  created() {
+    const shop: Shop = {
+      image: '/shop.jpeg',
+      shopName: 'ナクストコーヒー',
+      score: 3,
+      description:
+        '東京で人気沸騰中！リーズナブルな価格で本格的な入れたての美味しいコーヒーが飲めるお店。',
+    }
+
+    for (let i = 0; i < 9; i++) {
+      this.shops.push(shop)
+    }
+  },
+})
+</script>
+
+<style>
+.content-title {
+  font-size: 24px;
+  padding: 12px;
+}
+
+.shop-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
